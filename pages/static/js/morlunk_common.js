dffaabee0a339f9846a02b5f15db67d1b6f077fb
Andrew Comminos <andrewcomminos@gmail.com>
2012-06-02 22:17:49 -0700

    Fixed static resources not working.

---
+++ pages/static/js/morlunk_common.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+/*
+	$('.controlpane').hover(function() {
+		// Animate expansion to content size
+		// Use workaround here:
+		// http://stackoverflow.com/questions/5003220/javascript-jquery-animate-to-auto-height
+		var curHeight = $(this).height();
+		$(this).css({'height': 'auto'});
+		var autoHeight = $(this).height();
+		$(this).height(curHeight).stop().animate({
+			'height': autoHeight
+		});
+	}, function() {
+		// Contract to height of .panebanner
+		var panebannerHeight = $('.panebanner').height();
+		$(this).stop().animate({
+			'height': panebannerHeight
+		})
+	});
+*/
+});
