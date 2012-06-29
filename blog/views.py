8518d581a9ae1e0166efd9df94beb14c8e589a9a
Andrew Comminos <andrewcomminos@gmail.com>
2012-06-29 01:53:21 -0700

    Added support for JSON access to blog for Android app.

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,22 @@ from django.template import RequestContext, Template
 from django.template.loader import get_template
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, redirect
+from django.utils import simplejson
+from django.forms.models import model_to_dict
 
-def blog(request):
+def blog(request, format="html"):
 	posts = BlogPost.objects.order_by('-post_date') # Sort by date, descending.
-	return render_to_response('blog.html',
+	if format == "html":
+		return render_to_response('blog.html',
 								{ 'posts': posts },
 								RequestContext(request))
+	elif format == "json":
+		# JSONify everything
+		post_list = []
+		for post in posts:
+			# TODO switch to model_to_dict later.
+			post_list.append({"title": post.title, "description": post.description, "body": post.body})
+		return HttpResponse(simplejson.dumps({"posts" : post_list}), mimetype="application/json")
 
 def blog_post(request, post_id):
 	post = BlogPost.objects.get(id=post_id)
