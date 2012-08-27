5cf3a3100b0114ae3249731d9b69ed1bfceb0bce
Andrew Comminos <andrewcomminos@gmail.com>
2012-08-27 01:54:26 -0700

    Work on radio.

--- radio/urls.py
+++ radio/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('radio.views',
         (r'^$', 'show_radio'),
         (r'^start', 'start_playing'),
-        (r'^stop', 'start_playing'),
+        (r'^stop', 'stop_playing'),
         (r'^queue', 'queue_song'),
         )
